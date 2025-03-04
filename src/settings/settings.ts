22c08ad3308234c9f7137882b8590b5d059278a9
alesiakorzun <liskorzun@gmail.com>
2025-03-04 22:05:29 +0300

    add root file for notes

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -2,27 +2,34 @@ import { App, normalizePath, PluginSettingTab, Setting } from 'obsidian';
 import NotesPlugin from 'main';
 import { FolderSuggest } from 'ui/suggesters/FolderSuggester';
 import { FileSuggester } from '../ui/suggesters/FileSuggester';
-import { fileOrFolderExists } from '../utils';
+import { fileExists } from '../utils';
+import { fileCreateFromTemplate } from '../utils/fileCreateFromTemplate';
+import { rootNoteTemplate } from '../templates/rootNoteTemplate';
 
 
 export interface NotesSettings {
 	demoSetup: boolean;
 	noteTemplate: string;
 	notesFolder: string;
+	rootNotePath: string;
+
 }
 
 export const DEFAULT_SETTINGS: Partial<NotesSettings> = {
 	demoSetup: false,
 	noteTemplate: 'tech/templates/notes/note.md',
 	notesFolder: '',
+	rootNotePath: 'data/notes/root.md'
 }
 
 export class NotesSettingTab extends PluginSettingTab {
 	plugin: NotesPlugin;
+	rootNoteExists: boolean;
 
 	constructor(app: App, plugin: NotesPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
+		this.rootNoteExists = false;
 	}
 
 	display(): void {
@@ -33,6 +40,7 @@ export class NotesSettingTab extends PluginSettingTab {
 		});
 
 		this.switchDemoSetupSetting(containerEl);
+		this.addRootFileSetting(containerEl);
 		this.addNotesFolderSetting(containerEl);
 		this.addTemplateSetting(containerEl)
 
@@ -61,7 +69,39 @@ export class NotesSettingTab extends PluginSettingTab {
 				}));
 
 
+	}
 
+	private addRootFileSetting(containerEl: HTMLElement) {
+		const desc = document.createDocumentFragment();
+		desc.append(
+			'This is the root file which will be used as parent for cluster files.',
+			desc.createEl('br'),
+			'We recommend to use default.',
+			desc.createEl('br'),
+			'If it doesn\'t exist just create it.'
+		);
+		new Setting(containerEl)
+			.setName('Root file location')
+			.setDesc(desc)
+			.addSearch(search => {
+				new FileSuggester(this.app, search.inputEl);
+				search.setPlaceholder('Example: folder/file.md')
+					.setValue(this.plugin.settings.rootNotePath)
+					.onChange(async (newFile) => {
+						this.plugin.settings.rootNotePath = normalizePath(newFile);
+						this.rootNoteExists = await fileExists(this.app, newFile);
+						console.log(this.rootNoteExists);
+						await this.plugin.saveSettings();
+					})
+				// @ts-ignore
+				search.containerEl.addClass('templater_search');
+				// search.inputEl.focus();
+			})
+			.addButton(button =>{
+				button.setCta().setButtonText('Create').setDisabled(this.rootNoteExists).onClick(async() => {
+					await fileCreateFromTemplate(this.app, this.plugin.settings.rootNotePath, rootNoteTemplate);
+				})
+			})
 	}
 
 	private addNotesFolderSetting(containerEl: HTMLElement) {
---
+++ src/templates/rootNoteTemplate.ts
@@ -0,0 +1,7 @@
+export const rootNoteTemplate = `
+\`\`\`dataview
+LIST 
+FROM [[]] and !outgoing([[]]) 
+SORT order ASC
+\`\`\`
+`
